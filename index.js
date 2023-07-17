d060b27e4c27ca05b1fc9b26e2978402e2fba3c6
Juan Camilo Corrales Osvath <juancamilocorra@gmail.com>
2023-07-17 12:56:41 -0500

    fixed cart bug so when the user clicks on the cart logo on a mobile device it will be redirected to the cart page. also included the corresponding titles in the html files.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const hamb = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const close = document.getElementById("close");
 
-/* por que la condicion es si hamb? */
+
 if (hamb) {
     hamb.addEventListener('click', () =>{
         nav.classList.add('active');
